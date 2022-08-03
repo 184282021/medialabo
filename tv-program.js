807a1fbb9df13d877a53cfd786b847fe4b490055
184282021 <218428@st.takushoku-u.ac.jp>
2022-08-04 01:11:43 +0900

    rensyu

--- tv-program.js
+++ tv-program.js
@@ -81,92 +81,27 @@ let data = {
 
 /////////////////// 課題3-2 はここから書き始めよう
 
-//
+let s1 = document.querySelector('tbody#b1');
 
-/*let pro = data.list.g1[0].service.name;
-  let s = document.querySelector('span#pro');
-s.textContent = pro;
-  let pra = data.list.g1[0].title;
-  let d = document.querySelector('span#pra');
-d.textContent = pra;*/
-let bangumi;
-let b = document.querySelector('#print');
-b.addEventListener('click', kensaku);
 
-function kensaku(){
-  //URLを設定
 
-  //ジャンル選び
-  let i = document.querySelectorAll('input[name="janru"]');
-  let o = document.querySelector('input[name="janru"]');
-  let janru = o.value;
-  //番組選び
-  let p = document.querySelectorAll('input[name="bangumi"]');
+  
+function sendRequest(){
+  
+  const Cha = document.getElementById('channel').value;//検索欄のチャンネル名の値を取得
+  const Gan = document.getElementById('genre').value;//検索欄のジャンル名の値を取得
 
-  //if (typeof data.service.name === 'string') {
-    //let chan = JSON.parse(data.service.name);
-//}
-  //let bangumi;
-  for(let r of p){
-    if(r.checked){
-      bangumi = r.value;
-    }
-  }
-  let url ='https://www.nishita-lab.org/web-contents/jsons/nhk/'+ bangumi +'-'+janru+'-'+'j.json';
+  //urlを指定
+  let url='https://www.nishita-lab.org/web-contents/jsons/nhk/'+Cha+'-'+Gan+'-j.json';
+  
+  // 通信開始
 
   axios.get(url)
-  .then(showResult)   // 通信成功
+  .then(hantei)   // 通信成功
   .catch(showError)   // 通信失敗
-  .then(finish);  
-
-  console.log(janru);
-  console.log(bangumi);
+  .then(finish);      // 通信の最後の処理
 }
 
-function showResult(resp) {
-  // サーバから送られてきたデータを出力
-  let data = resp.data;
-  
-  //let i = document.querySelectorAll(data.list);
-  //console.log(i.length);
-
-  // data が文字列型なら，オブジェクトに変換する
-  if (typeof data === 'string') {
-      data = JSON.parse(data);
-  }
-   
-//if(typeof bangumi === "string"){
-    //  bangumi = JSON.parse(bangumi);
-//}
-
-  // data をコンソールに出力
-  console.log(data);
-  
-  if(bangumi === "e1"||bangumi === "g1"){
- for(let j = 0; j < 5; j++){
-  if(bangumi === "e1"){
-    //let pro = document.querySelector("data.list.e1["+ j +"].service.name");
-    let pro = data.list.e1[j].service.name;
-    console.log(pro);
-  let s = document.querySelector('span#pro');
-s.textContent = pro;
-  let pra = data.list.e1[j].title;
-  let d = document.querySelector('span#pra');
-d.textContent = pra;
-}else {
-  let pro = data.list.g1[j].service.name;
-  let s = document.querySelector('span#pro');
-s.textContent = pro;
-  let pra = data.list.g1[j].title;
-  let d = document.querySelector('span#pra');
-d.textContent = pra;
-}
-    }
-  }
-
-  // data.x を出力
-  //console.log(data.x);
-}
 
 
 
@@ -191,6 +126,12 @@ function hantei(resp) {
   for (let T of TR) {
     T.remove();
   }
+  
+
+
+
+
+
 if(data===null){
   let aaa = document.createElement('p');
   aaa.textContent ='検索結果に合う番組はありませんでした.';
@@ -234,7 +175,6 @@ if(data===null){
   }
 
   }
-
 
 
 function showError(err){
